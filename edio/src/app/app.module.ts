6463ab71f0808b50a9904f43643644a37e95a19c
Edio <educationstudio.io@gmail.com>
2018-04-08 06:46:35 -0500

    Setup an Angular5 project and installed bootstrap4 & Firebase

---
+++ edio/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { environment } from './../environments/environment';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    NgbModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ edio/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDxaznB2acAxj6F2N846QOYUzpRnnJmihA",
+    authDomain: "edio-33c81.firebaseapp.com",
+    databaseURL: "https://edio-33c81.firebaseio.com",
+    projectId: "edio-33c81",
+    storageBucket: "edio-33c81.appspot.com",
+    messagingSenderId: "272716375027"
+  }
+};
